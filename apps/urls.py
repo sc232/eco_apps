f2934903d2a0b2e9a46e35858d75fe34cef86ce6
Alexander Simoes <alexandersimoes+github@gmail.com>
2011-06-16 12:45:17 -0400

    Updated with new treemap library and proximity network

--- apps/urls.py
+++ apps/urls.py
@@ -10,6 +10,11 @@ urlpatterns = patterns('',
 	# Stacked
     (r'^stacked_share/$', 'apps.viz.views.stacked_share'),
     (r'^stacked_value/$', 'apps.viz.views.stacked_value'),
+	# Proximity
+    (r'^proximity_network/$', 'apps.viz.views.proximity_network'),
+    (r'^proximity_network/(?P<product_code>\d+)$', 'apps.viz.views.proximity_network'),
+
+	(r'^test/(?P<year>\d+)/(?P<path>.*)$', 'apps.viz.views.test'),
 
 	# Serve static files for dev
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/alexandersimoes/Sites/eco_apps/media', 'show_indexes': True}),
--- apps/viz/views.py
+++ apps/viz/views.py
@@ -1,16 +1,29 @@
+# For Django
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
 from apps.viz.models import *
-
+# For general Use
+from collections import defaultdict
 import json
+import time
 
 def home(request):
 	return render_to_response('index.html')
 
 # Treemaps
 def treemap_of_exports(request):
-	return render_to_response('treemap_of_exports.html')
+	import time
+	s = time.time()
+	c = Country.objects.get(name_3char='deu')
+	cpys = c.comtrade_cpys.filter(year=2009, rca__gte=1)
+	exports = defaultdict(lambda: [])
+	for cpy in cpys:
+		# exports[cpy.product.leamer.id].append(cpy.export_value)
+		exports[cpy.product.leamer.id].append((cpy.export_value, cpy.product.leamer.color))
+		
+	# return HttpResponse(time.time() - s)
+	# return HttpResponse(json.dumps(exports.values()))
+	return render_to_response('treemap_of_exports.html', {'data': json.dumps(exports.values())})
 
 def treemap_of_imports(request):
 	return render_to_response('treemap_of_imports.html')
@@ -46,7 +59,7 @@ def stacked_share(request):
 	# import time
 	# start = time.time()
 	# Query DB for a specific country
-	c = Country.objects.get(name_3char='nor')
+	c = Country.objects.get(name_3char='deu')
 	data = {}
 	years = [x for x in range(2001, 2010, 1)]
 	# Get all data for the given country in the given year
@@ -76,3 +89,60 @@ def stacked_share(request):
 
 def stacked_value(request):
 	return render_to_response('stacked_value.html')
+
+def proximity_network(request, product_code=None):
+	# Is this an ajax request (ie did we just double click a node?)
+	format = request.GET.get('format', False)
+	# What is the ID of the node we're getting data for?
+	try:
+		if format:
+			a_product = Sitc4.objects.get(pk=product_code)
+		else:	
+			a_product = Sitc4.objects.get(code=product_code)
+	except Sitc4.DoesNotExist:
+		a_product = Sitc4.objects.get(pk=78)
+	# Get all products so that we can add metadata (eg color, name, size) when we return it
+	products = Sitc4.objects.all()
+	# Assign to default to empty lists
+	product_dict_of_countries = defaultdict(lambda: [])
+	# Get all CPYs for given year and RCA threshold
+	cpys = Sitc4_comtrade_cpy.objects.filter(year = 2001, rca__gte = 1.2)#.values_list('product_id', 'product__name', 'product__leamer__color')
+	# Loop through each item and append country_ids to list whose key is the product
+	for cpy in cpys:
+		product_dict_of_countries[cpy.product_id].append(cpy.country_id)
+	#
+	my_set_of_countries = set(product_dict_of_countries[a_product.id])
+	# This will hold ALL the products with their proximities
+	prox_dict = {}
+	# For each product's list of countries see if there is any
+	# overlap (intersection) with the given product_id's list of countries, this
+	# will be the numerator. Also, for each product's list of countries find
+	# which has a longer length and use this as the denominator
+	for product_id, country_list in product_dict_of_countries.items():
+		# see if there is any intersection
+		if my_set_of_countries.intersection(set(country_list)):
+			# this will be the numerator i.e. the number of countries that both
+			# export the given product with RCA > given threshold
+			number_of_countries_in_intersection = len(my_set_of_countries.intersection(set(country_list)))
+			# find the max of the length of the two sets
+			# this will be the denominator in this specific proximity function
+			max_countries = max(len(my_set_of_countries), len(country_list))
+			# here we find the proximity
+			# WARNING: need to convert at least one value to a float so python will
+			# return floating point values
+			proximity = number_of_countries_in_intersection / float(max_countries)
+			# now add it to the dictionary EXCEPT if it is the given product
+			if product_id != a_product.id and proximity > 0.33:
+				this_product = products.get(pk=product_id)
+				prox_dict[product_id] = [this_product.name, this_product.code, this_product.leamer.color, proximity, this_product.ps_size]
+	# to be returned
+	# data = {}
+	# for product_id, proximity in prox_dict.items():
+	# 	if proximity > 0.3:
+	# 		data[product_id] = proximity
+	if format:
+		return HttpResponse(json.dumps(prox_dict))
+	return render_to_response('proximity_network.html', {'selected_product':a_product, 'data': json.dumps(prox_dict)})
+
+def test(request, year, path):
+	return HttpResponse(year, path)
